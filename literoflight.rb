91c31acf77f3f4117b89e3b3d5db32900b216d0f
Dylan M Fitzgerald <dylan@dylanfitzgerald.net>
2012-07-24 19:05:32 +0000

    Clean up layout and Sass. Add CC link & image.

--- literoflight.rb
+++ literoflight.rb
@@ -8,10 +8,12 @@ set :haml, :format => :html5, :layout => true
    # n.b. :layout => true renders haml docs through layout.haml if it exists
    # and can be redirected to another symbol for a different layout
    # or "false" for none
+
+# set up basic caching
+set :static_cache_control, [:public, :max_age => 300]
 before do
   cache_control :public, :must_revalidate, :max_age => 300
 end
-set :static_cache_control, [:public, :max_age => 300]
 
 get '/' do
   haml :main, :locals => { :text => markdown(:main), :title => "" }
